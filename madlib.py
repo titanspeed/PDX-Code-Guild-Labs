6ba2e5bfa0252a723f02f10fa578255acd9b916b
Chase Daniels <titanspeed@gmail.com>
2017-04-03 20:38:25 -0700

    Added madlib

---
+++ madlib.py
@@ -0,0 +1,18 @@
+#def money(amount):
+    #print("Here is ${}" .format(amount))
+
+#first_name = input('What is your first name?:')
+#last_name = input('What is your last name?:')
+
+
+
+animal = input('Hello mortal. I demand you give me the name of an animal:')
+food = input('Fascinating. Now give me a food:')
+movie = input('Now, minion, tell me a movie title:')
+celebrity = input('Rotten Potatoes gave that movie a 9%.\
+Tell me your favorite actor or actress:')
+
+print("""COMPILING DATA: \nListen, peon. We have video evidence of one ENORMOUS {} sitting
+on your lap while you were watching {}. If that's not
+bad enough, you were sitting next to {},
+and I am pretty sure you spilled {} all over the floor.""".format(animal, movie, celebrity, food))
